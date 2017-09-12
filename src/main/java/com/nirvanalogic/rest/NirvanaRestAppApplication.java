09c753988ce57c9991d0496e5873e70cb4f3ebb5
shashank-nl <shashank.nirvanalogic@gmail.com>
2017-09-12 01:48:42 -0700

    Initial Commit

---
+++ src/main/java/com/nirvanalogic/rest/NirvanaRestAppApplication.java
@@ -0,0 +1,12 @@
+package com.nirvanalogic.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirvanaRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirvanaRestAppApplication.class, args);
+	}
+}
